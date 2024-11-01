ee50122795584e06e8ec8fd1049d564ab46ba898
nikhil ahirwar <nikhilraj2908@gmail.com>
2024-11-01 16:23:06 +0530

    express 1st code

---
+++ code_express1st.js
@@ -0,0 +1,19 @@
+let http=require("http");
+let express=require("express");
+let app=express();
+
+app.get("/",(req,res)=>{
+    res.status(200).json("message:'get request on default'");
+})
+
+app.get("/demo",(req,res)=>{
+    res.status(200).json("message:'demo se aai request'");
+})
+
+app.post("/",(req,res)=>{
+    res.status(200).json("message:'default post request'");
+})
+
+app.listen(2021,()=>{
+    console.log("server on 2021");
+})
